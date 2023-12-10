259f35958802384d112c30ecc17cbee60b4e5695
Sebastian Vargas <vargaspizango1@gmail.com>
2023-12-10 17:18:50 -0500

    feat: add ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -96,15 +96,25 @@ export const getStaticPaths = async () => {
 
   return {
     paths: pokemonNames.map((name) => ({ params: { name } })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
+  const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
   return {
-    props: { pokemon: await getPokemonInfo(name) },
+    props: { pokemon },
   };
 };
 
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -93,14 +93,24 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths: pokemons.map((id) => ({ params: { id } as const })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
 
-  return { props: { pokemon: await getPokemonInfo(id) } };
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: { pokemon }, revalidate: 86400 };
 };
 
 export default PokemonPage;
